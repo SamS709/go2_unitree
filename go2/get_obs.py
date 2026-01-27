211f00519073a37c5bf2e1e680fcbc9b84f31f10
Sami LEROUX <sami.leroux75@gmail.com>
2026-01-27 17:10:44 +0100

    process heightmap changed

--- go2/get_obs.py
+++ go2/get_obs.py
@@ -44,7 +44,7 @@ def get_obs_low_state(
     """
     
     motor_states = lowstate_msg.motor_state[:12]
-
+    print(height_map[:,:,0])
     current_joint_pos_sdk = np.array([motor_states[i].q for i in range(12)])
     current_joint_vel_sdk = np.array([motor_states[i].dq for i in range(12)])
 
--- go2/lidar_utils.py
+++ go2/lidar_utils.py
@@ -6,6 +6,8 @@ Utility functions for processing LiDAR data and creating height maps.
 import numpy as np
 from unitree_sdk2_python.unitree_sdk2py.idl.sensor_msgs.msg.dds_ import PointCloud2_
 import struct
+import sys
+np.set_printoptions(precision=2, threshold=sys.maxsize, linewidth=2000)
 
 
 def pointcloud_to_heightmap(lidar_msg, grid_size=80, map_range=4.0):
@@ -89,7 +91,6 @@ def pointcloud_to_heightmap(lidar_msg, grid_size=80, map_range=4.0):
     return height_map, info
 
 def process_height_map(height_map: np.array, lidar_msg: PointCloud2_, max_dist: float, delete_count: int = 100):
-    
     grid_size = height_map.shape[0]
     map_range = 2.0 * max_dist
     cell_size = map_range / grid_size  # meters per cell
@@ -98,6 +99,11 @@ def process_height_map(height_map: np.array, lidar_msg: PointCloud2_, max_dist: 
     num_points = lidar_msg.width * lidar_msg.height
     point_step = lidar_msg.point_step
     data_bytes = bytes(lidar_msg.data)
+
+    # Clear old data (cells not updated in delete_count frames)
+    # old_cells = height_map[:, :, 1] > delete_count
+    # height_map[old_cells, 0] = 0.0  # Reset height
+    # height_map[old_cells, 1] = 0    # Reset age
     
     # Increment age for all cells (vectorized)
     height_map[:, :, 1] += 1
@@ -117,14 +123,11 @@ def process_height_map(height_map: np.array, lidar_msg: PointCloud2_, max_dist: 
         grid_x = int((x + max_dist) / cell_size)
         grid_y = int((y + max_dist) / cell_size)
         
-        if 0 <= grid_x < map_range and 0 <= grid_y < map_range:
+        if 0 <= grid_x < grid_size and 0 <= grid_y < grid_size:
             height_map[grid_x, grid_y, 0] = z
             height_map[grid_x, grid_y, 1] = 0
     
-    # Clear old data (cells not updated in delete_count frames)
-    old_cells = height_map[:, :, 1] > delete_count
-    height_map[old_cells, 0] = 0.0  # Reset height
-    height_map[old_cells, 1] = 0    # Reset age
+    
     
 
 def visualize_heightmap(height_map, info, show_full_stats=True):
